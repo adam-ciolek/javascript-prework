5e6ed3d6aa03676224b287b50397f3d535430213
Adam Ciołek <adasciolek@gmail.com>
2021-04-24 16:50:22 +0200

    Add buttons

--- js/script.js
+++ js/script.js
@@ -46,12 +46,29 @@ function displayResult(argPlayerMove, argComputerMove) {
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
 console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
+
 console.log('ruch gracza to: ' + playerMove);
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
+
+var argButtonName, buttonTest;
+
+/**
+ * Describe this function...
+ */
+function buttonClicked(argButtonName) {
+  clearMessages();
+  console.log(argButtonName + ' został kliknięty');
+  playerMove = argButtonName;
+}
+buttonTest = document.getElementById('button-rock');
+buttonTest.addEventListener('click', function(){ buttonClicked('kamień'); });
+buttonTest = document.getElementById('button-paper');
+buttonTest.addEventListener('click', function(){ buttonClicked('papier'); });
+buttonTest = document.getElementById('button-scissors');
+buttonTest.addEventListener('click', function(){ buttonClicked('nożyczki'); });
