39aded5cd20f97b183405c0e3df53b1984737fdc
Adam Ciołek <adasciolek@gmail.com>
2021-04-30 18:26:57 +0200

    Add change const

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
 //Gra
-let argButtonName,buttonRock,buttonPaper,buttonScissors;
 
 
-buttonRock = document.getElementById('button-rock');
-buttonPaper = document.getElementById('button-paper');
-buttonScissors = document.getElementById('button-scissors');
+
+const buttonRock = document.getElementById('button-rock');
+const buttonPaper = document.getElementById('button-paper');
+const buttonScissors = document.getElementById('button-scissors');
 
 
 /**
